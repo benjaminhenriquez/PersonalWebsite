a1e3773c4789ab31a80538ed95f91bd296d80f0d
benjaminhenriquez <henriquez.ben@gmail.com>
2017-08-30 10:48:39 -0400

    new version

--- public/resources/js/index.js
+++ public/resources/js/index.js
@@ -1,25 +1,25 @@
-function toggle() {
-  var h = document.getElementById("home");
-  var p = document.getElementById("projects");
-  var l = document.getElementById("left_click");
-  var r = document.getElementById("right_click");
-  h.style.display = (
-    h.style.display == "none" ? "block" : "none");
-    l.style.display = (
-      l.style.display == "none" ? "block" : "none");
-  p.style.display = (
-    p.style.display == "none" ? "block" : "none");
-    r.style.display = (
-      r.style.display == "none" ? "block" : "none");
-}
-
-
-function about(){
-  document.getElementById('ben_about').style.display = 'block';
-  document.getElementById('ben_photo').style.display = 'none';
-}
-
-function picture(){
-  document.getElementById('ben_about').style.display = 'none';
-  document.getElementById('ben_photo').style.display = 'block';
-}
+// function toggle() {
+//   var h = document.getElementById("home");
+//   var p = document.getElementById("projects");
+//   var l = document.getElementById("left_click");
+//   var r = document.getElementById("right_click");
+//   h.style.display = (
+//     h.style.display == "none" ? "block" : "none");
+//     l.style.display = (
+//       l.style.display == "none" ? "block" : "none");
+//   p.style.display = (
+//     p.style.display == "none" ? "block" : "none");
+//     r.style.display = (
+//       r.style.display == "none" ? "block" : "none");
+// }
+//
+//
+// function about(){
+//   document.getElementById('ben_about').style.display = 'block';
+//   document.getElementById('ben_photo').style.display = 'none';
+// }
+//
+// function picture(){
+//   document.getElementById('ben_about').style.display = 'none';
+//   document.getElementById('ben_photo').style.display = 'block';
+// }
